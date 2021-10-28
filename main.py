3975e820bea49a25ac02d1c74b4746c84e3abc07
SanjulaD <desanjula99@gmail.com>
2021-10-28 17:41:31 +0530

    feat: services added

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import fastapi
 import uvicorn
 
+from api import weather_api
+
 api = fastapi.FastAPI()
+
+
+def configure():
+    configure_routing()
+
+
+def configure_routing():
+    api.include_router(weather_api.router)
 
 
 @api.get('/')
@@ -10,4 +20,7 @@ def index():
 
 
 if __name__ == '__main__':
+    configure()
     uvicorn.run(api, port=8000, host='127.0.0.1')
+else:
+    configure()
